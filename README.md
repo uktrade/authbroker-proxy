18410ae0b7d1ce93f3bcb4b189ad193732e0434d
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2018-01-28 16:13:05 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# ABC proxy (initial POC) 
+
+### NOTES
+
+The proxy requires nginx compiled with support for the auth_request directive. This is available in the official nginx docker image.
+
+The access token should not be exposed beyond the proxy as we use the authorization code grant type, which is for machine-machine access.
+
+For every request made to the application, nginx first hits the auth_request url. If a 401 response is received, it redirects the user to the ABC login page.
+
+### Set-up
+
+You can run a local environment using docker-compose
+
+`cd proxy; cp .env.template .env`
+
+Put the OAuth2 client id, secret and url into the .env file.
+
+Run `docker-compose up`
+
+navigate to `http://{your-docker-ip}/` which should present you with the auth broker login screen
+
+Ensure that the correct redirect uri is whitelisted for your OAuth2 application, e.g `http://{your-docker-ip}/auth/response`
---
+++ proxy/app.py
@@ -0,0 +1,87 @@
+import os
+import datetime as dt
+import logging
+from urllib.parse import urljoin
+
+from flask import Flask, redirect, url_for, session, request, jsonify, abort
+from flask_oauthlib.client import OAuth
+
+from raven.contrib.flask import Sentry
+
+
+ABC_CLIENT_ID = os.getenv('ABC_CLIENT_ID')
+ABC_CLIENT_SECRET = os.getenv('ABC_CLIENT_SECRET')
+ABC_BASE_URL = os.getenv('ABC_BASE_URL')
+ABC_TOKEN_URL = urljoin(ABC_BASE_URL, '/o/token/')
+ABC_AUTHORIZE_URL = urljoin(ABC_BASE_URL, '/o/authorize/')
+ABC_INTROSPECT_URL = urljoin(ABC_BASE_URL, '/o/introspect/')
+
+PORT = int(os.getenv('PORT', 5000))
+
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
+app.debug = os.getenv('DEBUG') == 'True'
+oauth = OAuth(app)
+sentry = Sentry(app, dsn=os.getenv('SENTRY_DSN'))
+
+abc = oauth.remote_app(
+   'abc',
+   base_url=ABC_BASE_URL,
+   request_token_url=None,
+   access_token_url=ABC_TOKEN_URL,
+   authorize_url=ABC_AUTHORIZE_URL,
+   consumer_key=ABC_CLIENT_ID,
+   consumer_secret=ABC_CLIENT_SECRET,
+   access_token_method='POST'
+)
+
+
+@app.route('/auth/login')
+def login():
+    return abc.authorize(callback=url_for('authorized', _external=True))
+
+
+@app.route('/auth/check')
+def check():
+    """
+    Verify that a token is authenticated for use with nginx's auth_request directive.
+    This view should only return either a 202 or a 401 response.
+    """
+
+    # TODO: cache ABC response for specified time period, e.g. 1 minute
+    # to minimise number of calls to the ABC introspec view.
+
+    if 'abc_token' in session:
+
+        # Better to have a lightweight auth check endpoint on the ABC, rather than hitting 
+	# the profile url
+        me = abc.get('/api/v1/user/me/')
+        if me.status == 200:
+            return '', 202
+
+    abort(401)
+
+
+@app.route('/auth/response')
+def authorized():
+    resp = abc.authorized_response()
+    if resp is None or resp.get('access_token') is None:
+        abort(401)
+
+    session['abc_token'] = (resp['access_token'], '')
+
+    next_url = request.args.get('next', '/')
+
+    return redirect(next_url) 
+
+
+@abc.tokengetter
+def get_abc_oauth_token():
+    return session.get('abc_token')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=PORT)
+
